326a1c4a2ff7e02466716da7ced78dfab6649aad
LordMoMA <lihongyi0512@gmail.com>
2023-12-21 00:28:48 -0500

    “day1-part2”

--- day1.go
+++ day1.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
+
+// PART ONE
 
 var TextArray = []string{}
 
@@ -73,4 +76,61 @@ func calcSum(sumArray []int) int {
 		Result += v
 	}
 	return Result
+}
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+// PART TWO
+
+var SumArray2 = []int{}
+
+func extractCalibration2(line string) int {
+	nums := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	var firstDigit, lastDigit int
+	firstIndex, lastIndex := 0, len(line)
+
+	for word, num := range nums {
+		index := strings.Index(line, word)
+		if index != -1 && index > firstIndex {
+			firstIndex = index
+			firstDigit = num
+		}
+		if index != -1 && index < lastIndex {
+			lastIndex = index
+			lastDigit = num
+		}
+	}
+
+	for i := 1; i <= 9; i++ {
+		index := strings.Index(line, strconv.Itoa(i))
+		if index != -1 && index < firstIndex {
+			firstIndex = index
+			firstDigit = i
+		}
+		if index != -1 && index > lastIndex {
+			lastIndex = index
+			lastDigit = i
+		}
+	}
+	return firstDigit*10 + lastDigit
+
+}
+
+func formSum2(textArray []string) []int {
+	for _, v := range textArray {
+		SumArray2 = append(SumArray2, extractCalibration2(v))
+	}
+	return SumArray2
 }
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// day 1
+	// day1 part one
 	textArray := extractFile()
 	SumArray := formSum(textArray)
-	// fmt.Println(SumArray)
 	Result := calcSum(SumArray)
 	fmt.Println(Result)
+
+	// day1 part two
+	SumArray2 := formSum2(textArray)
+	// fmt.Println(SumArray2)
+	Result2 := calcSum(SumArray2)
+	fmt.Println(Result2)
 }

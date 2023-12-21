1b34b4372513665e534cc743283a47f9045a5c92
LordMoMA <lihongyi0512@gmail.com>
2023-12-21 14:10:34 -0500

    “day1-part2-alternative”

--- Day1/part2.go
+++ Day1/part2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"strings"
+	"unicode"
 )
 
 var nums = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
@@ -10,12 +11,12 @@ var nums = []string{"one", "two", "three", "four", "five", "six", "seven", "eigh
 func firstNumber(s string) int {
 	acc := ""
 
-	for i := 0; i < len(s); i++ {
-		if s[i] >= '0' && s[i] <= '9' {
-			return int(s[i] - '0')
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			return int(r - '0')
 		}
 
-		acc += string(s[i])
+		acc += string(r)
 
 		for i, d := range nums {
 			if strings.HasSuffix(acc, d) {
@@ -26,6 +27,48 @@ func firstNumber(s string) int {
 	log.Fatal("not found")
 	return 0
 }
+
+func lastNumber(s string) int {
+	acc := ""
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if unicode.IsDigit(rune(s[i])) {
+			return int(s[i] - '0')
+		}
+
+		acc = string(s[i]) + acc
+
+		for i, d := range nums {
+			if strings.HasPrefix(acc, d) {
+				return i + 1
+			}
+		}
+	}
+	log.Fatal("not found")
+	return 0
+}
+
+/*
+An alternative implementation of firstNumber and lastNumber
+func firstNumber(s string) int {
+	acc := ""
+
+	for i := 0; i < len(s); i++ {
+		if s[i] >= '0' && s[i] <= '9' {
+			return int(s[i] - '0')
+		}
+
+		acc += string(s[i])
+
+			for i, d := range nums {
+				if strings.HasSuffix(acc, d) {
+					return i + 1
+				}
+			}
+		}
+		log.Fatal("not found")
+		return 0
+	}
 
 func lastNumber(s string) int {
 	acc := ""
@@ -46,3 +89,4 @@ func lastNumber(s string) int {
 	log.Fatal("not found")
 	return 0
 }
+*/

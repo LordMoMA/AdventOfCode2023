4457c689f01c46118afe0bfec8b8aa726e34004e
LordMoMA <lihongyi0512@gmail.com>
2023-12-28 17:23:14 -0500

    “day3-part1”

---
+++ Day3/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ExtrctFileAndCalcSumNearSymbol(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+	sum := CalcSumNearSymbol(lines)
+	return sum, nil
+}
+
+/*
+(-1,-1) | (-1,0) | (-1,1)
+--------------------------
+(0,-1)  | (0,0)  | (0,1)
+--------------------------
+(1,-1)  | (1,0)  | (1,1)
+
+dx[0] and dy[0] (-1, -1) represent moving up-left.
+dx[1] and dy[1] (-1, 0) represent moving up.
+dx[2] and dy[2] (-1, 1) represent moving up-right.
+dx[3] and dy[3] (0, -1) represent moving left.
+dx[4] and dy[4] (0, 1) represent moving right.
+dx[5] and dy[5] (1, -1) represent moving down-left.
+dx[6] and dy[6] (1, 0) represent moving down.
+dx[7] and dy[7] (1, 1) represent moving down-right.
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+*/
+
+func CalcSumNearSymbol(lines []string) int {
+	dx := []int{-1, -1, -1, 0, 0, 1, 1, 1}
+	dy := []int{-1, 0, 1, -1, 1, -1, 0, 1}
+	total := 0
+	seen := make(map[string]bool)
+
+	for i := range lines {
+		for j := range lines[i] {
+			if lines[i][j] >= '0' && lines[i][j] <= '9' {
+				// Find the start of the number
+				start := j
+				for start > 0 && lines[i][start-1] >= '0' && lines[i][start-1] <= '9' {
+					start--
+				}
+				// Find the end of the number
+				end := j
+				for end < len(lines[i])-1 && lines[i][end+1] >= '0' && lines[i][end+1] <= '9' {
+					end++
+				}
+				numStr := lines[i][start : end+1]
+				num, _ := strconv.Atoi(numStr)
+				// Check all eight directions for a symbol
+				for k := range dx {
+					ni, nj := i+dx[k], start+dy[k] // check the start coordinate of the number
+					// if the character at the new coordinates is not a dot and not a digit (i.e., it's a symbol).
+					if ni >= 0 && ni < len(lines) && nj >= 0 && nj < len(lines[i]) && lines[ni][nj] != '.' && (lines[ni][nj] < '0' || lines[ni][nj] > '9') {
+						key := fmt.Sprintf("%d,%d", i, start)
+						if _, ok := seen[key]; !ok {
+							total += num
+							seen[key] = true
+						}
+						break
+					}
+					ni, nj = i+dx[k], end+dy[k] // check the end of the number
+					if ni >= 0 && ni < len(lines) && nj >= 0 && nj < len(lines[i]) && lines[ni][nj] != '.' && (lines[ni][nj] < '0' || lines[ni][nj] > '9') {
+						key := fmt.Sprintf("%d,%d", i, end)
+						if _, ok := seen[key]; !ok {
+							total += num
+							seen[key] = true
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+	return total
+}
+
+func main() {
+	// part one
+	sum, err := ExtrctFileAndCalcSumNearSymbol("day3.txt")
+	if err != nil {
+		panic(err)
+	}
+	println(sum)
+
+}
